9e9f5471612e3352c6a58b190fa77cd2e6df2a53
ByteGuy8 <>
2025-02-01 02:45:53 -0400

    Code style changes. Some fixes. Added an allocator

--- bstr.c
+++ bstr.c
@@ -5,9 +5,28 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int _bstr_grow(size_t new_len, struct bstr *str)
-{
-    unsigned char *buff = realloc((void *)str->buff, new_len);
+#define BSTR_SIZE (sizeof(BStr))
+#define BSTR_OUTOFSPACE(str_len, str) ((str)->used + (str_len) >= (str)->len)
+#define BSTR_APPENDLEN(str_len, str) ((str)->len + (str_len))
+#define BSTR_NEWLEN(str_len, str) (BSTR_APPENDLEN(str_len, str) + (BSTR_APPENDLEN(str_len, str) * 0.25) + 1)
+
+static void *lzalloc(size_t size, BStrAllocator *allocator){
+    return allocator ? allocator->alloc(size, allocator->ctx) : malloc(size);
+}
+
+static void *lzrealloc(void *ptr, size_t old_size, size_t new_size, BStrAllocator *allocator){
+    return allocator ? allocator->realloc(ptr, new_size, old_size, allocator->ctx) : realloc(ptr, new_size);
+}
+
+static void lzdealloc(void *ptr, size_t size, BStrAllocator *allocator){
+    if (!ptr) return;
+
+    if (allocator) allocator->dealloc(ptr, size, allocator->ctx);
+    else free(ptr);
+}
+
+static int grow(size_t new_len, BStr *str){
+    unsigned char *buff = lzrealloc((void *)str->buff, str->len, new_len, str->allocator);
 
     if (!buff)
         return 1;
@@ -18,32 +37,28 @@ int _bstr_grow(size_t new_len, struct bstr *str)
     return 0;
 }
 
-struct bstr *bstr_create_empty()
-{
-    struct bstr *str = (struct bstr *)malloc(sizeof(struct bstr));
+BStr *bstr_create_empty(BStrAllocator *allocator){
+    BStr *str = (BStr *)lzalloc(BSTR_SIZE, allocator);
 
-    if (!str)
-        return NULL;
+    if (!str) return NULL;
 
     str->len = 0;
     str->used = 0;
     str->buff = NULL;
+    str->allocator = allocator;
 
     return str;
 }
 
-struct bstr *bstr_create_append(char *raw_str)
-{
-    if (!raw_str)
+BStr *bstr_create_append(char *raw_str, BStrAllocator *allocator){
+    BStr *str = bstr_create_empty(allocator);
+
+    if (!raw_str || !str){
+        bstr_destroy(str);
         return NULL;
+    }
 
-    struct bstr *str = bstr_create_empty();
-
-    if (!str)
-        return NULL;
-
-    if (bstr_append(raw_str, str))
-    {
+    if (bstr_append(raw_str, str)){
         bstr_destroy(str);
         return NULL;
     }
@@ -51,50 +66,46 @@ struct bstr *bstr_create_append(char *raw_str)
     return str;
 }
 
-void bstr_destroy(struct bstr *str)
-{
-    if (!str)
-        return;
+void bstr_destroy(BStr *str){
+    if (!str) return;
 
-    if (str->buff)
-    {
+    BStrAllocator *allocator = str->allocator;
+
+    if (str->buff){
         memset(str->buff, 0, str->len);
-        free(str->buff);
+        lzdealloc(str->buff, str->len, allocator);
     }
 
-    memset(str, 0, sizeof(struct bstr));
-    free(str);
+    memset(str, 0, BSTR_SIZE);
+    lzdealloc(str, BSTR_SIZE, allocator);
 }
 
-#define BSTR_OUTOFSPACE(str_len, str) (str->used + str_len >= str->len)
-#define BSTR_APPENDLEN(str_len, str) (str->len + str_len)
-#define BSTR_NEWLEN(str_len, str) (BSTR_APPENDLEN(str_len, str) + (BSTR_APPENDLEN(str_len, str) * 0.25) + 1)
-
-int bstr_grow_by(size_t percentage, struct bstr *str)
-{
+int bstr_grow_by(size_t percentage, BStr *str){
     size_t len = str->used == 0 ? percentage : str->used * (percentage / 100.0);
-    return _bstr_grow(str->len + len, str);
+    return grow(str->len + len, str);
 }
 
-struct bstr *bstr_substr(size_t start, size_t end, struct bstr *str)
-{
+BStr *bstr_substr(size_t start, size_t end, BStr *str){
     char *buff = (char *)bstr_raw_substr(start, end, str);
+    BStr *new_str = bstr_create_append(buff, str->allocator);
 
-    if (!buff)
+    if (!buff || !new_str){
+        lzdealloc(buff, end - start + 2, str->allocator);
+        bstr_destroy(new_str);
         return NULL;
-
-    struct bstr *new_str = bstr_create_append(buff);
+    }
 
     memset(buff, 0, strlen(buff));
-    free(buff);
+    lzdealloc(buff, end - start + 2, str->allocator);
 
     return new_str;
 }
 
-unsigned char *bstr_raw_substr(size_t start, size_t end, struct bstr *str)
-{
+unsigned char *bstr_raw_substr(size_t start, size_t end, BStr *str){
     size_t len = end - start + 1;
-    unsigned char *buff = (unsigned char *)malloc(len + 1);
+    unsigned char *buff = (unsigned char *)lzalloc(len + 1, str->allocator);
+
+    if(!buff) return NULL;
 
     memcpy(buff, str->buff + start, len);
     buff[len] = 0;
@@ -102,16 +113,12 @@ unsigned char *bstr_raw_substr(size_t start, size_t end, struct bstr *str)
     return buff;
 }
 
-int bstr_append(char *raw_str, struct bstr *str)
-{
+int bstr_append(char *raw_str, BStr *str){
     size_t str_len = strlen(raw_str);
 
-    if (BSTR_OUTOFSPACE(str_len, str))
-    {
+    if (BSTR_OUTOFSPACE(str_len, str)){
         size_t new_len = BSTR_NEWLEN(str_len, str);
-
-        if (_bstr_grow(new_len, str))
-            return 1;
+        if (grow(new_len, str)) return 1;
     }
 
     memcpy(str->buff + str->used, raw_str, str_len);
@@ -122,19 +129,15 @@ int bstr_append(char *raw_str, struct bstr *str)
     return 0;
 }
 
-int bstr_append_args(struct bstr *str, char *format, ...)
-{
+int bstr_append_args(BStr *str, char *format, ...){
     va_list args;
     va_start(args, format);
 
     size_t str_len = vsnprintf(NULL, 0, format, args);
 
-    if (BSTR_OUTOFSPACE(str_len, str))
-    {
+    if (BSTR_OUTOFSPACE(str_len, str)){
         size_t new_len = BSTR_NEWLEN(str_len, str);
-
-        if (_bstr_grow(new_len, str))
-            return 1;
+        if (grow(new_len, str)) return 1;
     }
 
     va_start(args, format);
@@ -149,16 +152,13 @@ int bstr_append_args(struct bstr *str, char *format, ...)
     return 0;
 }
 
-int bstr_insert(size_t at, char *raw_str, struct bstr *str)
-{
+int bstr_insert(size_t at, char *raw_str, BStr *str){
     size_t str_len = strlen(raw_str);
 
-    if (BSTR_OUTOFSPACE(str_len, str))
-    {
+    if (BSTR_OUTOFSPACE(str_len, str)){
         size_t new_len = BSTR_NEWLEN(str_len, str);
 
-        if (_bstr_grow(new_len, str))
-            return 1;
+        if (grow(new_len, str)) return 1;
     }
 
     size_t left_len = at;
@@ -174,19 +174,16 @@ int bstr_insert(size_t at, char *raw_str, struct bstr *str)
     return 0;
 }
 
-int bstr_insert_args(size_t at, struct bstr *str, char *format, ...)
-{
+int bstr_insert_args(size_t at, BStr *str, char *format, ...){
     va_list args;
     va_start(args, format);
 
     size_t str_len = vsnprintf(NULL, 0, format, args);
 
-    if (BSTR_OUTOFSPACE(str_len, str))
-    {
+    if (BSTR_OUTOFSPACE(str_len, str)){
         size_t new_len = BSTR_NEWLEN(str_len, str);
 
-        if (_bstr_grow(new_len, str))
-            return 1;
+        if (grow(new_len, str)) return 1;
     }
 
     size_t left_len = at;
@@ -212,8 +209,7 @@ int bstr_insert_args(size_t at, struct bstr *str, char *format, ...)
     return 0;
 }
 
-void bstr_remove(size_t start, size_t end, struct bstr *str)
-{
+void bstr_remove(size_t start, size_t end, BStr *str){
     size_t len = end - start + 1;
     size_t right_len = str->used - end - 1;
 
--- bstr.h
+++ bstr.h
@@ -1,26 +1,29 @@
-#ifndef _BSTR_H_
-#define _BSTR_H_
+#ifndef BSTR_H
+#define BSTR_H
 
 #include <stddef.h>
 
-typedef struct bstr
-{
+typedef struct bstr_allocator{
+    void *ctx;
+    void *(*alloc)(size_t size, void *ctx);
+    void *(*realloc)(void *ptr, size_t new_size, size_t old_size, void *ctx);
+    void (*dealloc)(void *ptr, size_t size, void *ctx);
+}BStrAllocator;
+
+typedef struct bstr{
     size_t len;
     size_t used;         // without NULL character
     unsigned char *buff; // NULL terminated
+    struct bstr_allocator *allocator;
 } BStr;
 
-struct bstr *bstr_create_empty();
+BStr *bstr_create_empty(BStrAllocator *allocator);
+BStr *bstr_create_append(char *raw_str, BStrAllocator *allocator);
+void bstr_destroy(BStr *str);
 
-struct bstr *bstr_create_append(char *raw_str);
-
-void bstr_destroy(struct bstr *str);
-
-int bstr_grow_by(size_t percentage, struct bstr *str);
-
-struct bstr *bstr_substr(size_t start, size_t end, struct bstr *str);
-
-unsigned char *bstr_raw_substr(size_t start, size_t end, struct bstr *str);
+int bstr_grow_by(size_t percentage, BStr *str);
+BStr *bstr_substr(size_t start, size_t end, BStr *str);
+unsigned char *bstr_raw_substr(size_t start, size_t end, BStr *str);
 
 #define BSTR_CLONE(str)(bstr_create_append(str->buff))
 
@@ -28,14 +31,10 @@ unsigned char *bstr_raw_substr(size_t start, size_t end, struct bstr *str);
 #define BSTR_CHAR_AT(index, str) (str->buffer + index)
 #define BSTR_CODE(index, str) ((int)(str->buffer + index))
 
-int bstr_append(char *raw_str, struct bstr *str);
-
-int bstr_append_args(struct bstr *str, char *format, ...);
-
-int bstr_insert(size_t at, char *raw_str, struct bstr *str);
-
-int bstr_insert_args(size_t at, struct bstr *str, char *format, ...);
-
-void bstr_remove(size_t start, size_t end, struct bstr *str);
+int bstr_append(char *raw_str, BStr *str);
+int bstr_append_args(BStr *str, char *format, ...);
+int bstr_insert(size_t at, char *raw_str, BStr *str);
+int bstr_insert_args(size_t at, BStr *str, char *format, ...);
+void bstr_remove(size_t start, size_t end, BStr *str);
 
 #endif
